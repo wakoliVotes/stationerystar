7970895aefe2a5d9b8f35c4b7384ef1aefe670c7
wakoliVotes <votesdw@yahoo.com>
2026-07-03 11:19:53 +0300

    packages

---
+++ app/src/main/java/com/example/stationerystar/navigation/Screens.kt
@@ -0,0 +1,3 @@
+package com.example.stationerystar.navigation
+
+class Screens
---
+++ app/src/main/java/com/example/stationerystar/navigation/appnavigation.kt
@@ -0,0 +1,20 @@
+package com.example.stationerystar.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation () {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "splash_screen")
+    {
+        composable("splash_screen") {
+//            SplashScreen(navController)
+        }
+        composable("main_screen") {
+//            MainScreen()
+        }
+    }
+}
